6bc2984bf7b7022757cc0162f36c83936a8c0d8e
luciodiri <guy.avivi@gigya-inc.com>
2016-07-25 15:42:57 +0300

    fix include path and namespaces in cli/encryption

--- cli/encryption.php
+++ cli/encryption.php
@@ -6,19 +6,19 @@
  * Time: 3:49 PM
  */
 
-include_once "../src/GigyaApiHelper.php";
+include_once  __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "GigyaApiHelper.php";
 
 if ($argv[1] == "-e") {
-    $encStr = \gigya\GigyaApiHelper::enc($argv[2], $argv[3]);
+    $encStr = \Gigya\CmsStarterKit\GigyaApiHelper::enc($argv[2], $argv[3]);
 
     echo $encStr . PHP_EOL;
 } elseif ($argv[1] == "-d") {
-    $dec = \gigya\GigyaApiHelper::decrypt($argv[2], $argv[3]);
+    $dec = \Gigya\CmsStarterKit\GigyaApiHelper::decrypt($argv[2], $argv[3]);
 
     echo $dec . PHP_EOL;
 } elseif ($argv[1] == "-gen") {
     $str = isset($argv[2]) ? $argv[2] : null;
-    $key = \Gigya\GigyaApiHelper::genKeyFromString($str);
+    $key = \Gigya\CmsStarterKit\GigyaApiHelper::genKeyFromString($str);
 
     echo $key . PHP_EOL;
 }
