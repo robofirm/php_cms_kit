04b66c0d179f2ec0c44ad43ec8d6502914daf33e
Ynhockey <ynhockey@gmail.com>
2017-11-19 15:36:39 +0200

    Major change: Changed deprecated mcrypt library with OpenSSL. Also fixed related unit test.

--- tests/TestEncryption.php
+++ tests/TestEncryption.php
@@ -1,26 +1,27 @@
 <?php
-use Gigya\CmsStarterKit\GigyaApiHelper;
+	/* Requires setting include path to the package's src directory */
+	include_once 'GigyaApiHelper.php';
 
-/**
- * Created by PhpStorm.
- * User: Yaniv Aran-Shamir
- * Date: 4/7/16
- * Time: 8:47 PM
- */
-class TestEncryption extends PHPUnit_Framework_TestCase
-{
-    private $key;
+	use Gigya\CmsStarterKit\GigyaApiHelper;
 
-    public function testEnc()
-    {
-        $toEnc = "testing testing 123";
-        $encStr = GigyaApiHelper::enc($toEnc, $this->key);
-        $decStr = GigyaApiHelper::decrypt($encStr, $this->key);
-        $this->assertEquals($toEnc, trim($decStr));
-    }
+	/**
+	 * Created by PhpStorm.
+	 * User: Yaniv Aran-Shamir
+	 * Date: 4/7/16
+	 * Time: 8:47 PM
+	 */
+	class TestEncryption extends PHPUnit_Framework_TestCase
+	{
+		private $key;
 
-    protected function setUp()
-    {
-        $this->key = GigyaApiHelper::genKeyFromString("testGenKey");
-    }
-}
+		public function testEnc() {
+			$toEnc = "testing testing 123";
+			$encStr = GigyaApiHelper::enc($toEnc, $this->key);
+			$decStr = GigyaApiHelper::decrypt($encStr, $this->key);
+			$this->assertEquals($toEnc, trim($decStr));
+		}
+
+		protected function setUp() {
+			$this->key = GigyaApiHelper::genKeyFromString("testGenKey");
+		}
+	}
